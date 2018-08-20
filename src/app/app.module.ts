60ebb9820d0bd5ca64384ba9a740b88199a28af0
Gerardo <gmonsivaist@gmail.com>
2018-08-20 17:27:29 -0500

    se agregan mas componentes y un boton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccesComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccesComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  //template: '<app-server></app-server> <app-server></app-server>',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+allowNewServer:boolean = true;
+  constructor() {
+    setTimeout(() =>{
+      this.allowNewServer= false;
+    },2000)
+   }
+
+  ngOnInit() {
+  }
+
+}
